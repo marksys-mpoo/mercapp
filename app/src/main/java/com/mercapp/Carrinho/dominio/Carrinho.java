62341caa2b6e833808cb146316111acaa7f8e763
wellington luiz <wellingtonluiz123456@gmail.com>
2016-10-30 21:25:38 -0300

    A classe Carrinho.

---
+++ app/src/main/java/com/mercapp/Carrinho/dominio/Carrinho.java
@@ -0,0 +1,47 @@
+package com.mercapp.Carrinho.dominio;
+
+import com.mercapp.supermercado.dominio.Produtos;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wellington on 30/10/16.
+ */
+
+public class Carrinho {
+    private int id;
+    private double valorTotal;
+    private double descontos;
+//    private ArrayList<Produtos> produtos;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//    public double getValorTotal() {
+//        return valorTotal;
+//    }
+//
+//    public void setValorTotal(double valorTotal) {
+//        this.valorTotal = valorTotal;
+//    }
+//
+//    public double getDescontos() {
+//        return descontos;
+//    }
+//
+//    public void setDescontos(double descontos) {
+//        this.descontos = descontos;
+//    }
+//
+//    public ArrayList<Produtos> getProdutos() {
+//        return produtos;
+//    }
+//
+//    public void setProdutos(ArrayList<Produtos> produtos) {
+//        this.produtos = produtos;
+//    }
+}
