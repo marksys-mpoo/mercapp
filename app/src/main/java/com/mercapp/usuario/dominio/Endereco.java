5d23d44d77b6b2ed38cfc84273275fc851696acd
wellington luiz <wellingtonluiz123456@gmail.com>
2016-10-30 21:21:38 -0300

    adicionei as classes pessoa, carrinho emdereço.

---
+++ app/src/main/java/com/mercapp/usuario/dominio/Endereco.java
@@ -0,0 +1,73 @@
+package com.mercapp.usuario.dominio;
+
+/**
+ * Created by wellington on 30/10/16.
+ */
+
+public class Endereco {
+
+    private String id;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String endereco;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
---
+++ app/src/main/java/com/mercapp/usuario/dominio/Pessoa.java
@@ -0,0 +1,60 @@
+package com.mercapp.usuario.dominio;
+
+/**
+ * Created by wellington on 30/10/16.
+ */
+
+public class Pessoa {
+
+    private int id;
+    private String nome;
+//    private String telefone;
+//    private Endereco endereco;
+//    private Usuario usuario;
+//
+//    public Endereco getEndereco() {
+//        return endereco;
+//    }
+//
+//    public void setEndereco(Endereco endereco) {
+//        this.endereco = endereco;
+//    }
+//
+//    public Usuario getUsuario() {
+//        return usuario;
+//    }
+//
+//    public void setUsuario(Usuario usuario) {
+//        this.usuario = usuario;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getTelefone() {
+//        return telefone;
+//    }
+//
+//    public void setTelefone(String telefone) {
+//        this.telefone = telefone;
+//    }
+//
+
+
+
+
+
+}
