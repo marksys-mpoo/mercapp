547cb50b93e9fe6a1c222ae1596f91c8e66b7c8a
jpereira <jobsonrp@gmail.com>
2016-12-16 23:03:45 -0300

    Alteração na lista de todos os produtos na tela de Supermercado.

--- app/src/main/java/com/mercapp/supermercado/gui/ListaProdutosDoSupermercado.java
+++ app/src/main/java/com/mercapp/supermercado/gui/ListaProdutosDoSupermercado.java
@@ -68,8 +68,8 @@ public class ListaProdutosDoSupermercado extends AppCompatActivity {
         Integer idSupermercado = session.getSupermercadoSelecionado().getId();
         String idSupermercado_string = idSupermercado.toString();
         Cursor cursor = buscaProdutos.listaProdutosDoSupermercado(idSupermercado_string);
-        String[] nomeCampos = new String[] {BDHelper.COLUNA_ID_PRODUTO, BDHelper.COLUNA_DESCRICAO_PRODUTO, BDHelper.COLUNA_PRECO_PRODUTO};
-        int[] idViews = new int[] { R.id.colunaProduto2, R.id.colunaProduto3, R.id.colunaProduto4};
+        String[] nomeCampos = new String[] {BDHelper.COLUNA_IMAGEM_PRODUTO, BDHelper.COLUNA_DESCRICAO_PRODUTO, BDHelper.COLUNA_PRECO_PRODUTO};
+        int[] idViews = new int[] { R.id.iconProduto, R.id.colunaProduto2, R.id.colunaProduto3};
         SimpleCursorAdapter adaptador = new SimpleCursorAdapter(_context,R.layout.produtos,cursor,nomeCampos,idViews, 0);
         lista = (ListView)findViewById(R.id.lista_produtos_do_supermercado);
         lista.setAdapter(adaptador);
