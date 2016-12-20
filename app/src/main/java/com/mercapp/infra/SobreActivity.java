b894f446286b1e8ce08781b6399b96a6be67226f
Wellington <wjfilmagens@hotmail.com>
2016-12-20 14:06:30 -0200

    texto justificado tela sobre e supermercado

--- app/src/main/java/com/mercapp/infra/SobreActivity.java
+++ app/src/main/java/com/mercapp/infra/SobreActivity.java
@@ -3,18 +3,31 @@ package com.mercapp.infra;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 import com.mercapp.R;
 import com.mercapp.usuario.gui.TelaMenuActivity;
 
+import static com.mercapp.R.string.descricao_tela_sobre;
+
 public class SobreActivity extends AppCompatActivity {
 
+    private String descricaoTelaSobre = "O projeto MercApp  tem como objetivo permitir que " +
+            "o cliente realize suas compras em poucos minutos pelo seu Smartphone." +
+            " O cliente irá buscar um supermercado de sua preferência que esteja cadastrado no " +
+            "aplicativo, selecionar os produtos, a forma de pagamento e receber tudo no conforto de sua casa." +
+            " Neste contexto os clientes terão mais tempo para fazer outras coisas no dia-a-dia ao " +
+            "invés de perder tempo no supermercado, já o supermercado não terá que contratar mais funcionários.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+        justificarTextoSobre();
     }
+
+
+
 
     public void onBackPressed() {
         Intent voltarMenu = new Intent(this, TelaMenuActivity.class);
@@ -22,5 +35,11 @@ public class SobreActivity extends AppCompatActivity {
         finish();
     }
 
+    public void justificarTextoSobre() {
+        WebView view = (WebView) findViewById(R.id.sobreDescricao);
+        String text;
+        text = "<html><body><p align=\"justify\">"+descricaoTelaSobre+"</p></body></html>";
+        view.loadData(text,"text/html;charset=UTF-8",null);
+    }
 
 }
